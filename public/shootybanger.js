1c126f48d6c62ae5ee97c75a90a905e245540891
Pi Lanningham <QRebound@gmail.com>
2016-02-09 19:48:07 -0500

    Linter suggestions

--- public/shootybanger.js
+++ public/shootybanger.js
@@ -1,37 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
     var socket = new WebSocket("ws://ricci:8089/socket", "echo-protocol");
-    
+
     socket.onopen = function() {
-        console.log("Connected.")
+        console.log("Connected.");
     };
-    
+
     socket.onmessage = function(event) {
         console.log("Got data from server: " + event.data);
     };
-    
+
     var key2key = function(keyCode) {
         // left arrow
         if (keyCode == 37)
             return "left";
-        
+
         // right arrow
         if (keyCode == 39)
             return "right";
-        
+
         // z
         if (keyCode == 90)
             return "jump";
-        
+
         // x
         if (keyCode == 88)
             return "shoot";
-        
+
         return null;
-    }
-    
+    };
+
     var sendKey = function(keyCode, act) {
         var key = key2key(keyCode);
-        
+
         if (key) {
             socket.send(JSON.stringify({
                 "kind": "key",
@@ -40,14 +40,14 @@ document.addEventListener("DOMContentLoaded", function() {
                     "act": act
                 }
             }));
-            console.log("Sent key " + key + " as " + act + ".")
+            console.log("Sent key " + key + " as " + act + ".");
         }
     };
-    
+
     document.addEventListener("keydown", function(evt) {
         sendKey(evt.keyCode, "down");
     });
-    
+
     document.addEventListener("keyup", function(evt) {
         sendKey(evt.keyCode, "up");
     });

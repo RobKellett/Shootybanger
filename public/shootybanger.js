82bd3e61b7258be089be537fe57333836205f347
Rob Kellett <Rob Kellett>
2016-02-09 19:45:43 -0500

    Send key events to server

--- public/shootybanger.js
+++ public/shootybanger.js
@@ -1,13 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
     var socket = new WebSocket("ws://ricci:8089/socket", "echo-protocol");
-
+    
     socket.onopen = function() {
-        socket.send("[hacker voice] I'm in.");
+        console.log("Connected.")
     };
-
+    
     socket.onmessage = function(event) {
-        var div = document.createElement("div");
-        div.textContent = event.data;
-        document.querySelector("body").appendChild(div);
+        console.log("Got data from server: " + event.data);
     };
+    
+    var key2key = function(keyCode) {
+        // left arrow
+        if (keyCode == 37)
+            return "left";
+        
+        // right arrow
+        if (keyCode == 39)
+            return "right";
+        
+        // z
+        if (keyCode == 90)
+            return "jump";
+        
+        // x
+        if (keyCode == 88)
+            return "shoot";
+        
+        return null;
+    }
+    
+    var sendKey = function(keyCode, act) {
+        var key = key2key(keyCode);
+        
+        if (key) {
+            socket.send(JSON.stringify({
+                "kind": "key",
+                "data": {
+                    "key": key,
+                    "act": act
+                }
+            }));
+            console.log("Sent key " + key + " as " + act + ".")
+        }
+    };
+    
+    document.addEventListener("keydown", function(evt) {
+        sendKey(evt.keyCode, "down");
+    });
+    
+    document.addEventListener("keyup", function(evt) {
+        sendKey(evt.keyCode, "up");
+    });
 });

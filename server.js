9d76a5362b395fba50a941d938d64f4179afe581
Pi Lanningham <QRebound@gmail.com>
2016-02-09 21:01:15 -0500

    Very stupid simple player position updates based on keypresses

--- server.js
+++ server.js
@@ -6,22 +6,62 @@ var ws = require('express-ws')(app);
 function connection(ws) {
   this.socket = ws;
   this.handle = handshake;
+  this.player = {
+    x: 0,
+    y: 0,
+    vx: 0
+  };
+
+  var me = this;
+
+  this.sendStateTimer = setInterval(function() {
+    try {
+      me.socket.send(JSON.stringify(me.player));
+    } catch (e) {
+      console.log(e);
+      this.close();
+    }
+  }, 200);
+  this.playerUpdateTimer = setInterval(function() {
+    me.player.x += me.player.vx;
+  }, 100);
 }
+connection.prototype.close = function() {
+  clearInterval(this.sendStateTimer);
+  clearInterval(this.playerUpdateTimer);
+  this.socket.close();
+};
+
 function handshake(message) {
-  if(message.kind === 'new') {
-    // Create a new game
-    this.socket.send('New game created with id ' + guid.raw());
-    this.handle = echo;
-  } else if(message.kind === 'join') {
-    this.socket.send('Joined game with id ' + message.game_id);
-    this.handle = echo;
-  }
-  else {
-    this.socket.send('Unexpected message');
+  try {
+    if(message.kind === 'new') {
+      // Create a new game
+      this.socket.send('New game created with id ' + guid.raw());
+      this.handle = game;
+    } else if(message.kind === 'join') {
+      this.socket.send('Joined game with id ' + message.game_id);
+      this.handle = game;
+    }
+    else {
+      console.log('Unexpected message: ' + JSON.stringify(message));
+      this.socket.send('Unexpected message');
+    }
+  } catch (e) {
+    console.log(e);
+    this.close();
   }
 }
-function echo(message) {
-  this.socket.send('echo: ' + JSON.stringify(message));
+
+function game(message) {
+  if(message.kind === 'key') {
+    if(message.data.act === 'up') {
+      this.player.vx = 0;
+    } else if(message.data.key === 'left') {
+      this.player.vx = -50;
+    } else if(message.data.key === 'right') {
+      this.player.vx = 50;
+    }
+  }
 }
 
 var port = 8089;
@@ -33,14 +73,13 @@ app.ws('/socket', function(ws, req) {
   var conn = new connection(ws);
 
   ws.on('message', function(msg) {
-    console.log('Received websocket message: ' + msg);
     try {
       var message = JSON.parse(msg);
       conn.handle(message);
     } catch (e) {
       console.log(e);
-      ws.send('Invalid message received');
-      ws.close();
+      conn.socket.send('Error processing message');
+      conn.close();
     }
   });
 });

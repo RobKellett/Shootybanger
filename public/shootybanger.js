4c85a3454faa4a8f16892390a504e2d86a700694
Rob Kellett <Rob Kellett>
2016-02-09 21:00:11 -0500

    Square

--- public/shootybanger.js
+++ public/shootybanger.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     var socket = new WebSocket("ws://ricci:8089/socket", "echo-protocol");
+    var canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var ctx = canvas.getContext("2d");
+    
+    var player = {
+        position: {
+            x: 0,
+            y: 0
+        },        
+        velocity: {
+            x: 0,
+            y: 0
+        }
+    };
 
     socket.onopen = function() {
         console.log("Connected.");
@@ -9,7 +24,10 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 
     socket.onmessage = function(event) {
-        console.log("Got data from server: " + event.data);
+        var data = JSON.parse(event.data);
+        player.position.x = data.x;
+        player.position.y = data.y;
+        player.velocity.x = data.vx;
     };
 
     var key2key = function(keyCode) {
@@ -54,4 +72,20 @@ document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("keyup", function(evt) {
         sendKey(evt.keyCode, "up");
     });
+    
+    window.addEventListener("resize", function() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    });
+    
+    var draw = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        ctx.fillStyle = "black";
+        ctx.fillRect(player.position.x, player.position.y, 100, 100);
+        
+        window.requestAnimationFrame(draw);
+    };
+    
+    window.requestAnimationFrame(draw);
 });
